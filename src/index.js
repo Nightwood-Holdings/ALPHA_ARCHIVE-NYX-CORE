4a982e6282c13e7e30314f505679fb46a7e6d7ca
Nightwood-Holdings <james.r.nightwood@gmail.com>
2026-06-02 09:16:11 +0000

    Add minimal Node.js scaffold and fix README/CHANGELOG

---
+++ src/index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import { NyxCore } from './nyxCore.js';
+
+const app = new NyxCore();
+app.start();
---
+++ src/nyxCore.js
@@ -0,0 +1,10 @@
+export class NyxCore {
+  constructor() {
+    this.version = '0.1.0';
+  }
+
+  start() {
+    console.log(`Nyx Core v${this.version} is running.`);
+    console.log('This repository is now buildable and runnable.');
+  }
+}
